0fd23493ee8c0ddc94e6a7af0f49113fce99e721
zdimension <zippedfire@free.fr>
2016-02-24 14:51:08 +0100

    work

---
+++ FreeRaider/FreeRaider/GLFont.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeRaider
+{
+    public class CharInfo
+    {
+        public uint TexIndex;
+
+        public int Width;
+
+        public int Height;
+
+        public int Left;
+
+        public int Top;
+
+        public float TexX0;
+
+        public float TexY0;
+
+        public float TexX1;
+
+        public float TexY1;
+
+        public float AdvanceX;
+
+        public float AdvanceY;
+    }
+
+    public class FontTexture
+    {
+        public FTLib
+    }
+}
